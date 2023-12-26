70d0c7b910b40ee5b925868fcd9918a578b87e35
sonjiseokk <thswltjr11@gmail.com>
2023-12-26 09:27:50 +0900

    Initial commit

---
+++ src/test/java/com/meta/instagram/InstagramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meta.instagram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstagramApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
